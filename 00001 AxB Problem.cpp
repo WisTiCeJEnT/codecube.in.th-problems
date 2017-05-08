8e5b3579d125ae9262841fb4cc69370c3436a96c
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-05-08 22:35:50 +0700

    00001

---
+++ 00001 AxB Problem.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+//#include <math.h>
+//#include <stdlib.h>
+//#include <iomanip>
+//#include <vector>
+
+
+using namespace std;
+
+int main()
+{
+	long a,b;
+	cin >> a >> b;
+	cout << a * b;
+	return 0;
+}
