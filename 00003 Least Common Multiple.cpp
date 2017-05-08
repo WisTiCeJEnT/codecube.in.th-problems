0246c368cfef2099cd82d101b7a54ded8b2fe552
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-05-08 22:19:21 +0700

    fix 00003

---
+++ 00003 Least Common Multiple.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+long gcd(long a,long b)
+{
+	if(b==0)
+		return a;
+	else
+		return gcd(b,a%b);
+}
+int main()
+{
+	long a,b;
+	cin >> a >> b;
+	cout << a / gcd(a,b) * b;
+	return 0;
+}
