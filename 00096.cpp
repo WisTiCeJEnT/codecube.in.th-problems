31b9ac742f5a8c0b7a8b15a9509d9f0774d227eb
Wattanai Sathuphan <nine.stay27@gmail.com>
2018-06-18 10:05:08 +0700

    debugging

--- 00096.cpp
+++ 00096.cpp
@@ -15,6 +15,7 @@ int main()
     int atk[n];
     int def[n];
     int ans[n];
+    int sum[n*2];
     for(int i=0;i<n;i++)
     {
         cin >> atk[i];
@@ -23,25 +24,50 @@ int main()
     {
         cin >> def[i];
     }
+    int j = n/2+1;
+    sum[0] = def[j];
+    for(int i=1;i<n*2;i++)
+    {
+        j++;
+        if(j>=n)
+            j-=n;
+        sum[i] = sum[i-1]+def[j];
+        //cout << j << " " << sum[i] << endl;
+    }
+    /*
+    while(true)
+    {
+        int a,b;
+        cin >> a >> b;
+        cout << sum[(n/2)+b]-sum[(n/2)+a] << endl;
+    }*/
+    //==================
+    int max = (n-1)/2;
     for(int i=0;i<n;i++)
     {
-        ans[i] = 0;
-        int l = ind(i-1);
-        int r = ind(i+1);
-        int ll = i;
-        while(l!=r and ll!=r)
+        //cout << "on " << i << endl;
+        int tmpAns = max/2;
+        int mn = 0;
+        int mx = max;
+        while(mx-mn>0)
         {
-            atk[i]-=(def[ind(l)] + def[ind(r)]);
-            if(atk[i]>=0)
+            int t1 = sum[(n/2)+i+tmpAns]; 
+            int t2 = sum[(n/2)+i-tmpAns-1]+def[i]; 
+            //cout << t1 << " " << t2 << " " << t1-t2 << endl; 
+            //cout << i << " try " <<tmpAns<< mn << " " << mx << endl;
+            if(t1-t2 <= atk[i])
             {
-                ans[i]+=2;
-                ll = l;
-                l = ind(l-1);
-                r = ind(r+1);
+                mn = tmpAns;
+                tmpAns = (mx+mn+1)/2;
             }
             else
-                break;
+            {
+                //cout << "else";
+                mx = tmpAns-1;
+                tmpAns = (mx+mx)/2;
+            }
         }
+        ans[i] = mn*2;
     }
     for(int i=0;i<n;i++)
         cout << ans[i] << " ";
